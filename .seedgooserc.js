1d43f5b809a3aa0305b50aa76513bd103ece267c
ttriizz <86247100+ttriizz@users.noreply.github.com>
2023-03-29 13:14:48 -0400

    1.2

--- .seedgooserc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    modelBaseDirectory: "app_server/database/models",
-    models: ["*.js", "!db.js"],
-    data: "data",
-    db: "mongodb://127.0.0.1:27017/travlr?authSource=admin",
-};
