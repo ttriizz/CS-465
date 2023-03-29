4a68e00b3a72c8b41ebc8ea45770833424b05d8e
ttriizz <86247100+ttriizz@users.noreply.github.com>
2023-03-29 13:10:02 -0400

    1.0

--- Travlr/app.js
+++ Travlr/app.js
@@ -4,11 +4,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 var hbs = require('hbs');
+require('./app_api/database/db');
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
-
+const apiRouter = require('./app_api/routes/index');
 const app = express();
 
 // view engine setup
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
